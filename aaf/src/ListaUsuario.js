f69c967828aed12475ef342ed88cc0008e4c873c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-26 11:43:55 -0300

    arrancando con lista usuarios y bdd corregida

--- aaf/src/ListaUsuario.js
+++ aaf/src/ListaUsuario.js
@@ -1,18 +1,26 @@
-/*import { Fragment, useState } from 'react';
+import { Fragment, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Registrar.css';
 import {Link} from 'react-router-dom'
 import {useNavigate} from 'react-router-dom'
+import Card from 'react-bootstrap/Card';
 import axios from 'axios';
-import { useState } from 'react';
+//import { useState } from 'react';
 
 function ListaUsuario(){
-    const [usuarios,setUsuarios] = useState({}); 
-    axios.get()
-    return (
-        
-    );  
-}
+    const [Usuarios, setUsuarios] = useState([{}]); 
+    axios.get('http://localhost:5000/aaf/getUsers')
+    .then(res => {
+        setUsuarios(res)
+      })
+      .catch(e => {
+      }); 
+      /*return ({Usuarios.map(Usuarios =><div className='usuarios'><Card nombreMascota={cita.mascota} nombreDueño={cita.dueño} fecha={cita.fecha} hora={cita.hora} sintomas={cita.sintomas}></Card>         
+      <button onClick={() => eliminarCita(citas.indexOf(cita))} class='button elimnar u-full-width'>Eliminar</button> </div>)}  
+      );*/  
+    }
+   
+    
 
-export default ListaUsuario*/
+export default ListaUsuario
